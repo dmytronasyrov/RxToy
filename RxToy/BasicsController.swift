d98bd8681ed839c6c1e239e1d46bb22efa568d37
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-03-31 23:33:15 +0300

    add variable bindings; cleanup

--- RxToy/BasicsController.swift
+++ RxToy/BasicsController.swift
@@ -32,6 +32,8 @@ class BasicsController: UIViewController {
     @IBOutlet weak var datePicker: UIDatePicker!
     
     lazy var disposeBag = DisposeBag()
+    let tapped = PublishSubject<String>()
+    let input = Variable<String?>("")
     
     // Life
     
@@ -39,14 +41,24 @@ class BasicsController: UIViewController {
         super.viewDidLoad()
         
         addTapKeyboardDismiss(tapRecognizer, view: view, bag: disposeBag)
-        linkLabelToField(inputField, label: inputLabel, bag: disposeBag)
-        linkLabelToTextView(textView, label: textViewLabel, bag: disposeBag)
-        linkLabelToBt(infoBt, label: btLabel, bag: disposeBag)
-        linkLabelToSegment(segment, label: segmentLabel, bag: disposeBag)
-        linkProgressToSlider(slider, progress: progress, bag: disposeBag)
-        linkActivityToSwitch(switcher, activity: activity, bag: disposeBag)
-        linkLabelToStepper(stepper, label: multiLabel, bag: disposeBag)
-        linkLabelToDatePicker(datePicker, label: multiLabel, bag: disposeBag)
+        linkLabelTo(field: inputField, label: inputLabel, bag: disposeBag)
+        linkLabelTo(textView: textView, label: textViewLabel, bag: disposeBag)
+        linkLabelTo(bt: infoBt, label: btLabel, bag: disposeBag)
+        linkLabelTo(segment: segment, label: segmentLabel, bag: disposeBag)
+        linkProgressTo(slider: slider, progress: progress, bag: disposeBag)
+        linkActivityTo(switcher: switcher, activity: activity, bag: disposeBag)
+        linkLabelTo(stepper: stepper, label: multiLabel, bag: disposeBag)
+        linkLabelTo(datePicker: datePicker, label: multiLabel, bag: disposeBag)
+        
+        linkFieldTo(variable: input, field: inputField, bag: disposeBag)
+        onChange(variable: input, bag: disposeBag) {
+            print("Input said: \(String(describing: $0))")
+        }
+        
+        publishButtonTo(subject: tapped, bt: infoBt, bag: disposeBag)
+        subscribeOn(subject: tapped, bag: disposeBag) {
+            print("Button said: \($0)\n")
+        }
     }
     
     // Private
@@ -57,13 +69,13 @@ class BasicsController: UIViewController {
             .disposed(by: bag)
     }
     
-    private func linkLabelToField(_ field: UITextField, label: UILabel, bag: DisposeBag) {
+    private func linkLabelTo(field: UITextField, label: UILabel, bag: DisposeBag) {
         field.rx.text.asDriver()
             .drive(label.rx.text)
             .disposed(by: bag)
     }
     
-    private func linkLabelToTextView(_ textView: UITextView, label: UILabel, bag: DisposeBag) {
+    private func linkLabelTo(textView: UITextView, label: UILabel, bag: DisposeBag) {
         textView.rx.text
             .bind(onNext: {
                 label.text = "Char count: \($0?.count ?? 0)"
@@ -71,7 +83,7 @@ class BasicsController: UIViewController {
             .disposed(by: bag)
     }
     
-    private func linkLabelToBt(_ bt: UIButton, label: UILabel, bag: DisposeBag) {
+    private func linkLabelTo(bt: UIButton, label: UILabel, bag: DisposeBag) {
         bt.rx.controlEvent(.touchUpInside).asDriver()
             .drive(onNext: {
                 label.text! += "Tapped. "
@@ -84,7 +96,7 @@ class BasicsController: UIViewController {
             .disposed(by: bag)
     }
     
-    private func linkLabelToSegment(_ segment: UISegmentedControl, label: UILabel, bag: DisposeBag) {
+    private func linkLabelTo(segment: UISegmentedControl, label: UILabel, bag: DisposeBag) {
         segment.rx.value.asDriver()
             .skip(1)
             .drive(onNext: {
@@ -93,13 +105,13 @@ class BasicsController: UIViewController {
             .disposed(by: bag)
     }
     
-    private func linkProgressToSlider(_ slider: UISlider, progress: UIProgressView, bag: DisposeBag) {
+    private func linkProgressTo(slider: UISlider, progress: UIProgressView, bag: DisposeBag) {
         slider.rx.value.asDriver()
             .drive(progress.rx.progress)
             .disposed(by: bag)
     }
     
-    private func linkActivityToSwitch(_ switcher: UISwitch, activity: UIActivityIndicatorView, bag: DisposeBag) {
+    private func linkActivityTo(switcher: UISwitch, activity: UIActivityIndicatorView, bag: DisposeBag) {
         switcher.rx.value.asDriver()
             .map { !$0 }
             .drive(activity.rx.isHidden)
@@ -110,15 +122,15 @@ class BasicsController: UIViewController {
             .disposed(by: bag)
     }
     
-    private func linkLabelToStepper(_ stepper: UIStepper, label: UILabel, bag: DisposeBag) {
+    private func linkLabelTo(stepper: UIStepper, label: UILabel, bag: DisposeBag) {
         stepper.rx.value.asDriver()
             .map { String(Int($0)) }
             .drive(label.rx.text)
             .disposed(by: bag)
     }
     
-    private func linkLabelToDatePicker(_ picker: UIDatePicker, label: UILabel, bag: DisposeBag) {
-        picker.rx.date.asDriver()
+    private func linkLabelTo(datePicker: UIDatePicker, label: UILabel, bag: DisposeBag) {
+        datePicker.rx.date.asDriver()
             .map { (date: Date) -> String in
                 let formatter = DateFormatter()
                 formatter.dateStyle = .full
@@ -128,6 +140,28 @@ class BasicsController: UIViewController {
                 label.text = $0
             }, onCompleted: nil, onDisposed: nil)
             .disposed(by: bag)
+    }
+    
+    private func linkFieldTo(variable: Variable<String?>, field: UITextField, bag: DisposeBag) {
+        field.rx.text.bind(to: variable).disposed(by: bag)
+    }
+    
+    private func publishButtonTo(subject: PublishSubject<String>, bt: UIButton, bag: DisposeBag) {
+        bt.rx.tap
+            .map { "Tapped!" }
+            .bind(to: subject)
+            .disposed(by: bag)
+    }
+    
+    private func subscribeOn<T>(subject: PublishSubject<T>, bag: DisposeBag, handler: ((T) -> Void)?) {
+        subject.subscribe(onNext: handler, onError: nil, onCompleted: nil, onDisposed: nil)
+            .disposed(by: bag)
+    }
+    
+    private func onChange<T>(variable: Variable<T>,  bag: DisposeBag, handler: ((T) -> Void)?) {
+        variable.asObservable()
+            .subscribe(onNext: handler, onError: nil, onCompleted: nil, onDisposed: nil)
+            .disposed(by: disposeBag)
     }
 }
 
